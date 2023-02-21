112cafca1f8db3168966643093c0df6b2f875ed2
Marcos Rodrigues <85518327+Marocosz@users.noreply.github.com>
2023-02-21 15:46:51 -0300

    Create testes.py

---
+++ testes.py
@@ -0,0 +1,81 @@
+def resultado(lista):
+
+    if lista[0] == 1:
+        if lista[1] == 1:
+            if lista[2] == 1:
+                return 'jogador X ganhou'
+
+    if lista[3] == 1:
+        if lista[4] == 1:
+            if lista[5] == 1:
+                return 'jogador X ganhou'
+
+    if lista[6] == 1:
+        if lista[7] == 1:
+            if lista[8] == 1:
+                return 'jogador X ganhou'
+
+    if lista[0] == 1:
+        if lista[3] == 1:
+            if lista[6] == 1:
+                return 'jogador X ganhou'
+
+    if lista[1] == 1:
+        if lista[4] == 1:
+            if lista[7] == 1:
+                return 'jogador X ganhou'
+
+    if lista[2] == 1:
+        if lista[5] == 1:
+            if lista[8] == 1:
+                return 'jogador X ganhou'
+
+    if lista[0] == 1:
+        if lista[4] == 1:
+            if lista[8] == 1:
+                return 'jogador X ganhou'
+
+    if lista[2] == 1:
+        if lista[4] == 1:
+            if lista[6] == 1:
+                return 'jogador X ganhou'
+
+    if lista[0] == 2:
+        if lista[1] == 2:
+            if lista[2] == 2:
+                return 'jogador O ganhou'
+
+    if lista[3] == 2:
+        if lista[4] == 2:
+            if lista[5] == 2:
+                return 'jogador O ganhou'
+
+    if lista[6] == 2:
+        if lista[7] == 2:
+            if lista[8] == 2:
+                return 'jogador O ganhou'
+
+    if lista[0] == 2:
+        if lista[3] == 2:
+            if lista[6] == 2:
+                return 'jogador O ganhou'
+
+    if lista[1] == 2:
+        if lista[4] == 2:
+            if lista[7] == 2:
+                return 'jogador O ganhou'
+
+    if lista[2] == 2:
+        if lista[5] == 2:
+            if lista[8] == 2:
+                return 'jogador O ganhou'
+
+    if lista[0] == 2:
+        if lista[4] == 2:
+            if lista[8] == 2:
+                return 'jogador O ganhou'
+
+    if lista[2] == 2:
+        if lista[4] == 2:
+            if lista[6] == 2:
+                return 'jogador O ganhou'
